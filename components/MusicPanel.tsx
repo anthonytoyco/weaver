81f2c62d460880e326aaaacc3937f5c7a9eac78f
Anthony Toyco <amieltoyco@gmail.com>
2025-07-19 10:49:28 +0000

    merged from old repo

---
+++ components/MusicPanel.tsx
@@ -0,0 +1,23 @@
+import Song from "./Song";
+
+function MusicPanel() {
+  const songs = [
+    { title: "AnySong", description: "some description" },
+    { title: "AnySong1", description: "another description" },
+    { title: "AnySong2", description: "new description" },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2 w-full bg-black">
+      {songs.map((song) => (
+        <Song
+          songTitle={song.title}
+          artist={song.description}
+          image="/default-image.png"
+        />
+      ))}
+    </div>
+  );
+}
+
+export default MusicPanel;
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MusicPanel from "../components/MusicPanel";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-row p-2 gap-2 w-screen h-screen bg-black">
+      <div className="flex flex-col w-1/3 p-2 gap-2 h-full bg-[#121212] rounded">
+        <div className="flex color-white font-light text-xl justify-around text-white">
+          WEAVER
+        </div>
+        <div className="w-full h-1/2 bg-black rounded"></div>
+        <div className="flex justify-around w-full">
+          <button className="flex justify-around items-center w-1/2 h-10 bg-green-50 rounded-full">
+            UPLOAD
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <MusicPanel />
+    </div>
+  );
 }
 
-export default App
+export default App;
