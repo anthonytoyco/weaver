e4b74bd5518192b961374ffa2262152e13aacf05
Andre Zamudio <zamudioandre2506@gmail.com>
2025-07-20 05:53:38 -0400

    Imporoved ui

--- app/components/Song.tsx
+++ app/components/Song.tsx
@@ -1,20 +1,34 @@
+import { useState } from "react";
 interface SongProps {
   image: string;
   songTitle: string;
   artist: string;
 }
 
+
 function Song(props: SongProps) {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    setMousePos({ x: event.clientX, y: event.clientY });
+  };
+
+  const gradientStyle = {
+    background: `radial-gradient(circle at ${mousePos.x}px ${mousePos.y}px, #feb47b, #5e77c4)`,
+  };
+
   return (
-    <div className="song flex justify-evenly rounded items-center bg-white">
-      <div className="bg-white w-1/4">
-        <img src={props.image} alt="song image" />
+    <div onMouseOver={handleMouseMove} style={gradientStyle} className="flex-row song gap-2 flex w-full h-1/5 p-2 rounded items-center bg-[#191932]">
+      <div className="text-white aspect-square w-1/6 font-light bg-black rounded-2xl">
+        <img src={props.image} alt="" />
       </div>
-      <div className="bg-white w-1/4">
-        <p>{props.songTitle}</p>
-      </div>
-      <div className="bg-white w-1/4">
-        <p>{props.artist}</p>
+      <div className="flex flex-col w-1/2 h-1/1">
+        <div className="text-white w-1/4 font-light ">
+          <p>{props.songTitle}</p>
+        </div>
+        <div className="text-white font-light w-full text-sm">
+          <p>{props.artist}</p>
+        </div>
       </div>
     </div>
   );
